14204a01469848d6f63e53d6361eee5200f25586
Kaitlyn W <kaitlnl27@gmail.com>
2023-07-05 14:20:03 -0400

    Update PB inverters

---
+++ pb_inverter.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity pb_inverter is
+	port 
+    (
+		pb_n : in  std_logic_vector(3 downto 0);
+		pb   : out std_logic_vector(3 downto 0)
+	);
+end pb_inverter;
+
+
+architecture gates of pb_inverter is
+
+begin
+	pb <= not(pb_n);
+
+end gates;
